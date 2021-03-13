62bc136725d6e93f1a09210e1487d08c5605d0f2
Anoymous Errors <zuner.hunter@gmail.com>
2021-03-13 13:35:37 +0200

    Add project files.

---
+++ TabUI.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace tabs
+{
+    partial class TabUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabs = new System.Windows.Forms.FlowLayoutPanel();
+            this.addtab = new System.Windows.Forms.Button();
+            this.Editor = new System.Windows.Forms.RichTextBox();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabs
+            // 
+            this.tabs.AutoSize = true;
+            this.tabs.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.tabs.Controls.Add(this.addtab);
+            this.tabs.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tabs.Location = new System.Drawing.Point(0, 0);
+            this.tabs.Name = "tabs";
+            this.tabs.Size = new System.Drawing.Size(800, 29);
+            this.tabs.TabIndex = 0;
+            // 
+            // addtab
+            // 
+            this.addtab.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.addtab.FlatAppearance.BorderSize = 0;
+            this.addtab.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.addtab.ForeColor = System.Drawing.Color.White;
+            this.addtab.Location = new System.Drawing.Point(3, 3);
+            this.addtab.Name = "addtab";
+            this.addtab.Size = new System.Drawing.Size(23, 23);
+            this.addtab.TabIndex = 0;
+            this.addtab.Text = "+";
+            this.addtab.UseVisualStyleBackColor = false;
+            this.addtab.Click += new System.EventHandler(this.addtab_click);
+            // 
+            // Editor
+            // 
+            this.Editor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.Editor.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Editor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Editor.ForeColor = System.Drawing.Color.White;
+            this.Editor.Location = new System.Drawing.Point(0, 29);
+            this.Editor.Name = "Editor";
+            this.Editor.Size = new System.Drawing.Size(800, 421);
+            this.Editor.TabIndex = 1;
+            this.Editor.Text = "";
+            // 
+            // TabUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Editor);
+            this.Controls.Add(this.tabs);
+            this.Name = "TabUI";
+            this.Text = "TabUI";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.TabUI_FormClosing);
+            this.Load += new System.EventHandler(this.TabUI_Load);
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel tabs;
+        private System.Windows.Forms.Button addtab;
+        private System.Windows.Forms.RichTextBox Editor;
+    }
+}
+
---
+++ TabUI.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabs
+{
+    public partial class TabUI : Form
+    {
+        public TabUI()
+        {
+            InitializeComponent();
+        }
+        public string current_tab = "Script1";
+        public int total_tabs = 0;
+        string tabspath = Application.StartupPath + "\\Scripts";
+        private void TabUI_Load(object sender, EventArgs e)
+        {
+            foreach (var item in Directory.GetFiles(tabspath))
+            {
+                total_tabs++;
+                addtab.Visible = false;
+                addtab.Parent = this;
+                maketab(total_tabs, tabs);
+                addtab.Parent = tabs;
+                addtab.Visible = true;
+                if (item == tabspath + "\\Script1")
+                {
+                    Editor.Text = File.ReadAllText(item);
+                }
+            }
+        }
+        private void addtab_click(object sender, EventArgs e)
+        {
+            object[] args = new string[0];
+            StreamWriter meow = new StreamWriter(tabspath + "\\" + current_tab);
+            meow.Close();
+            File.WriteAllText(tabspath + "\\" + current_tab, Editor.Text);
+            total_tabs++;
+            meow = new StreamWriter(tabspath + "\\Script" + total_tabs);
+            meow.Close();
+            Editor.Text = "";
+            addtab.Visible = false;
+            addtab.Parent = this;
+            maketab(total_tabs, tabs);
+            addtab.Parent = tabs;
+            addtab.Visible = true;
+            current_tab = "Script" + total_tabs;
+        }
+
+
+        private void tabclick(object sender, EventArgs e)
+        {
+            string txtoplace = "";
+            File.WriteAllText(tabspath + "/" + current_tab, Editor.Text);
+            string[] arr = sender.ToString().Split(' ');
+            if (arr[2] == current_tab) { return; }
+            foreach (var itm in Directory.GetFiles(tabspath))
+            {
+                if (Path.GetFileName(itm) == arr[2])
+                {
+                    txtoplace = File.ReadAllText(itm);
+                }
+            }
+            Editor.Text = txtoplace;
+            current_tab = arr[2];
+        }
+        private void Close_Click(object sender, EventArgs e)
+        {
+            string[] arr = sender.ToString().Split(' ');
+            tabs.Controls.Find("tab" + arr[3], false)[0].Parent = null;
+            total_tabs--;
+            for (var i = 0; i < total_tabs; i++)
+            {
+                var ind = i + 1;
+                tabs.Controls[i].Controls[0].Text = "Script" + ind;
+            }
+            var end = 1;
+            foreach (string item in Directory.GetFiles(tabspath))
+            {
+                if ((tabspath + "\\Script" + arr[3]) != item)
+                {
+                    File.Move(item, tabspath + "\\Script" + end);
+                    end++;
+                }
+                else { File.Delete(item); }
+            }
+        }
+        private void TabUI_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            File.WriteAllText(tabspath + "\\" + current_tab, Editor.Text);
+        }
+        void maketab(int tab_number, FlowLayoutPanel parent)
+        {
+            Panel tab_background = new Panel();
+            Button tab_switch = new Button();
+            Button tab_close = new Button();
+            var defcolor = Color.FromArgb(55, 55, 55);
+            // tab
+            tab_background.BorderStyle = BorderStyle.FixedSingle;
+            tab_background.Controls.Add(tab_switch);
+            tab_background.Controls.Add(tab_close);
+            tab_background.Dock = DockStyle.Left;
+            tab_background.Location = new Point(0, 0);
+            tab_background.Name = "tab" + tab_number;
+            tab_background.Size = new Size(80, 23);
+            tab_background.TabIndex = 1;
+            tab_background.Margin = new Padding(0);
+            // tab_switch
+            tab_switch.BackColor = defcolor;
+            tab_switch.FlatAppearance.MouseOverBackColor = defcolor;
+            tab_switch.FlatAppearance.MouseDownBackColor = defcolor;
+            tab_switch.ForeColor = Color.FromArgb(255, 255, 255);
+            tab_switch.Dock = DockStyle.Left;
+            tab_switch.FlatAppearance.BorderSize = 0;
+            tab_switch.FlatStyle = FlatStyle.Flat;
+            tab_switch.Font = new Font("Consolas", 7.5F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(204)));
+            tab_switch.Location = new Point(0, 0);
+            tab_switch.Name = "tab" + tab_number + "click";
+            tab_switch.Size = new Size(55, 23);
+            tab_switch.TabIndex = 0;
+            tab_switch.Text = "Script" + tab_number;
+            tab_switch.Padding = new Padding(0, 2, 0, 0);
+            tab_switch.TextAlign = ContentAlignment.MiddleCenter;
+            tab_switch.UseVisualStyleBackColor = false;
+            tab_switch.Click += tabclick;
+            // tab_close
+            tab_close.BackColor = defcolor;
+            tab_close.FlatAppearance.MouseOverBackColor = defcolor;
+            tab_close.FlatAppearance.MouseDownBackColor = defcolor;
+            tab_close.ForeColor = Color.FromArgb(255, 255, 255);
+            tab_close.Dock = DockStyle.Right;
+            tab_close.FlatAppearance.BorderSize = 0;
+            tab_close.FlatStyle = FlatStyle.Flat;
+            tab_close.Font = new Font("Consolas", 9F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(204)));
+            tab_close.Location = new Point(63, 0);
+            tab_close.Name = "tab" + tab_number + "close";
+            tab_close.Size = new Size(25, 23);
+            tab_close.TabIndex = 1;
+            tab_close.Text = "x " + tab_number;
+            tab_close.Padding = new Padding(0, 2, 0, 0);
+            tab_close.TextAlign = ContentAlignment.MiddleCenter;
+            tab_close.UseVisualStyleBackColor = false;
+            tab_close.Click += Close_Click;
+
+            parent.Controls.Add(tab_background);
+        }
+
+    }
+}
